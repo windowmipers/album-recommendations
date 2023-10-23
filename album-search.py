862888e6236b49670e6d70bcabd418b1ecba193c
windowmipers <milesajaxon@gmail.com>
2023-10-23 16:48:32 -0700

    wrote a search function for each parameter

--- album-search.py
+++ album-search.py
@@ -1,17 +1,151 @@
 import csv
 
 database = {}
+data_list = []
 with open("albums.csv", "r", newline=" ") as file:
     csv_reader = csv.DictReader(file)
     for row in csv_reader:
         album_name = row["Album Name"]
         genres = row["Genres"].split(", ")
-        ranking = row["Ranking"]
-        year = row["Year"]
-        data = [ranking].extend([year].extend(genres))
+        ranking = int(row["Ranking"])
+        year = int(row["Year"])
+        data = (ranking, year, genres)
         database[data] = album_name
 
-def main():
-    print("What do you want to search by? year/ranking/genre"
-    parameter = input()
+def year_search():
+    global data_list
+    while True:
+        print("Enter a year or a range of years, e.g. 1980 or 1980-1989:")
+        years = input()
+        years = years.strip()
+        try:
+            year = int(years)
+            span = False
+            break
+        except:
+            pass
+        try:
+            spl = years.split("-")
+            year1 = int(spl[0])
+            year2 = int(spl[1])
+            span = True
+            break
+        except:
+            pass
+        print("Please enter a valid year or range.")
+    if data_list == []:
+        if span:
+            for item in database:
+                if year1 <= item[1] <= year2:
+                    data_list.append(item)
+        else:
+            for item in database:
+                if item[1] == year:
+                    data_list.append(item)
+    else:
+        if span:
+            if not year1 <= item[1] <= year2:
+                data_list.remove(item)
+        else:
+            if item[1] != year:
+                data_list.remove(item)
+
+def ranking_search():
+    global data_list
+    print("Enter a number between 1 and 100 or a range of numbers, e.g. 30 or 11-20:")
+    while True:
+        rankings = input()
+        rankings = rankings.strip()
+        try:
+            rank = int(rankings)
+            span = False
+            break
+        except:
+            pass
+        try:
+            spl = rankings.split("-")
+            rank1 = int(spl[0])
+            rank2 = int(spl[1])
+            span = True
+            break
+        except:
+            pass
+        print("Please enter a valid ranking or range.")
+    if data_list == []:
+        if span:
+            for item in database:
+                if item[0] in range(rank1, rank2 + 1):
+                    data_list.append(item)
+        else:
+            for item in database:
+                if item[0] == rank:
+                    data_list.append(item)
+    else:
+        if span:
+            if item[0] not in range(rank1, rank2 + 1):
+                data_list.remove(item)
+        else:
+            if item[0] != rank:
+                data_list.remove(item)
+
+def genre_search():
+    global data_list
+    g = []
+    while True:
+        print("Enter a genre of music:")
+        genre = input()
+        genre = genre_format(genre)
+        g.append(genre)
+        while True:
+            print("Enter another genre? y/n")
+            answer = input()
+            answer = answer.lower()
+            if answer == "y" or answer == "n":
+                break
+        if answer == "y":
+            continue
+        if answer == "n":
+            break
+    for item in database:
+        matches = 0
+        for gen in item[2]:
+            if genre_format(gen) == genre:
+                matches += 1
+        if matches == len(g):
+            data_list.append(item)
     
+def genre_format(genre):
+    genre = genre.strip()
+    genre = genre.lower()
+    genre = genre.replace("-", " ")
+    return genre
+
+
+
+def search():
+    remaining_parameters = ["year", "ranking", "genre"]
+    first = True
+    while remaining_parameters != []:
+        parameters_display = "/".join(remaining_parameters)
+        if first:
+            print(f"What do you want to search by? {parameters_display}")
+        else:
+            print(f"Do you want to narrow the results by a different parameter? To end search and show results, type end. {parameters_display}/end")
+        parameter = input()
+        if parameter == "year" and "year" in remaining_parameters:
+            year_search()
+            remaining_parameters.remove("year")
+            first = False
+        elif parameter == "ranking" and "ranking" in remaining_parameters:
+            ranking_search()
+            remaining_parameters.remove("ranking")
+            first = False
+        elif parameter == "genre" and "genre" in remaining_parameters:
+            genre_search()
+            remaining_parameters.remove("genre")
+            first = False
+        elif parameter == "end":
+            break
+
+def show_results():
+    pass
